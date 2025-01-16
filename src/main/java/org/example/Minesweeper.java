88bc153fd22506c859f0757cea459fa39ea9066d
Adam-Zayd Saeed <adam-zayd.saeed@student.manchester.ac.uk>
2025-01-16 12:25:18 +0000

    Added Driver and Grid classes

--- src/main/java/org/example/Minesweeper.java
+++ src/main/java/org/example/Minesweeper.java
@@ -2,8 +2,9 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Minesweeper {
     public static void main(String[] args) {
-        //do somethingg
+        //create game instance
+        //run game instance on driver
         }
     }
